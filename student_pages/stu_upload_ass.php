01e15b055707b3e83b97d565ddfa2beecfec9c75
vasudha0309 <48402640+vasudha0309@users.noreply.github.com>
2021-02-01 22:47:00 +0530

    Update Navbar

--- student_pages/stu_upload_ass.php
+++ student_pages/stu_upload_ass.php
@@ -219,11 +219,10 @@ select{
     <div class="navbar" id="myTopnav">
     <a class="active" href="module_page.php"><i class="fa fa-file-text" aria-hidden="true"></i> Module</a>
     <a href="learning_homepage.php"><i class="fa fa-user  fa-home"></i> Home</a>      
-    <a href="download_resources.php"><i class="fa fa-download" aria-hidden="true"></i> Download learning material</a> 
-    <a href="stu_upload_ass.php"><i class="fa fa-upload" aria-hidden="true"></i> Upload Documents </a>
-        <a href="view_ass.php"><i class="fa fa-eye" aria-hidden="true"></i> Manage Documents </a>
-    <a href="stu_home.php"><i class="fa fa-files-o" aria-hidden="true"></i> Test module</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <a href="download_resources.php"><i class="fa fa-download" aria-hidden="true"></i> Download Assignments</a> 
+    <a href="stu_upload_ass.php"><i class="fa fa-upload" aria-hidden="true"></i> Upload Assignments </a>
+        <a href="view_ass.php"><i class="fa fa-eye" aria-hidden="true"></i> Manage Your Uploaded Assignments </a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"></i>
   </a>
       </div>
